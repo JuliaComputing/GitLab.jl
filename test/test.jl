58cd4b8491b17fd72ef82215ab8e835c206bfa39
Pradeep Mudlapur <pradeep@juliacomputing.com>
2016-07-26 18:23:22 +0530

    removed unwanted file

--- test/test.jl
+++
@@ -1,3 +0,0 @@
-import GitHub
-myauth = GitHub.authenticate(ENV["GITHUB_AUTH"]) # don't hardcode your access tokens!
-GitHub.star("JuliaWeb/GitHub.jl"; auth = myauth)  # star the GitHub.jl repo as the user identified by myauth
